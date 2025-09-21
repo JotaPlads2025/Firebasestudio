d3da5de172a86e4b4aa7d8c6c89a2aa153970dac
Jose Pablo Ballesteros Urteaga <j.ballesteros.u@gmail.com>
2025-09-21 00:07:37 +0000

    Try fixing this error: `Runtime Error: Error: Invariant failed: Could no

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -119,7 +119,7 @@ export default function DashboardPage() {
                   tickLine={false}
                   axisLine={false}
                   tickMargin={8}
-                  tickFormatter={(value) => `$${'${value / 1000}'}k`}
+                  tickFormatter={(value) => `$${value / 1000}k`}
                 />
                 <ChartTooltip
                   cursor={false}
