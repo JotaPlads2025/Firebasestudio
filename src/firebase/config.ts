28120557c63595315ea0fc51c6811a968fc93fc2
Jose Pablo Ballesteros Urteaga <j.ballesteros.u@gmail.com>
2025-10-18 20:20:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4308693113-a4650",
+  "appId": "1:605294250474:web:c73a10104fbda2275dca93",
+  "apiKey": "AIzaSyB0T2W0ElfQXrRhAANgMHBucgLSeESoPnY",
+  "authDomain": "studio-4308693113-a4650.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "605294250474"
+};
