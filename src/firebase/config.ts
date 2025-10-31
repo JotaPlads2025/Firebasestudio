009807525f0a137fc2a18cf5af66ae850029d56a
Jose Pablo Ballesteros Urteaga <j.ballesteros.u@gmail.com>
2025-10-31 20:04:21 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,23 @@
+
+// This file is machine-generated - edit with care!
+
+/**
+ * It is not recommended to edit this file directly. Instead, create a `.env` file in the root of the project
+ * and add your Firebase configuration there.
+ *
+ * This file is updated automatically when you deploy your app to Firebase App Hosting.
+ *
+ * Use the `firebase` command to interact with your Firebase project.
+ * @see https://firebase.google.com/docs/cli
+ *
+ * Use the `useFirebaseConfig` hook to get the Firebase configuration for the current environment.
+ * @see /src/firebase/use-firebase-config.ts
+ */
 export const firebaseConfig = {
-  "projectId": "studio-4308693113-a4650",
-  "appId": "1:605294250474:web:c73a10104fbda2275dca93",
-  "apiKey": "AIzaSyB0T2W0ElfQXrRhAANgMHBucgLSeESoPnY",
-  "authDomain": "studio-4308693113-a4650.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "605294250474"
+  apiKey: "AIzaSyB0T2W0ElfQXrRhAANgMHBucgLSeESoPnY",
+  appId: "1:605294250474:web:c73a10104fbda2275dca93",
+  authDomain: "studio-4308693113-a4650.firebaseapp.com",
+  messagingSenderId: "605294250474",
+  projectId: "studio-4308693113-a4650",
+  measurementId: "G-06PSD4LLPL",
 };
