3c4474c59fa2c3a1c4b7513763c3948cbb30156c
Jose Pablo Ballesteros Urteaga <j.ballesteros.u@gmail.com>
2025-11-13 13:47:48 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { Providers } from './providers';
+import { poppins, ptSans } from './fonts';
+import { cn } from '@/lib/utils';
 
 export const metadata: Metadata = {
   title: 'Plads Pro',
@@ -16,23 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="font-body bg-background text-foreground antialiased">
+      <body
+        className={cn(
+          'font-body bg-background text-foreground antialiased',
+          poppins.variable,
+          ptSans.variable
+        )}
+      >
         <Providers>{children}</Providers>
         <Toaster />
       </body>
